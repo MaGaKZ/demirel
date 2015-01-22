7d1a23b389f3af3e17b93fc3ba454d11bbccba17
MaGaKZ <shokparbayev@gmail.com>
2015-01-22 18:25:37 +0600

    Added links to subject_show

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,7 @@
+$( document ).ready(function() {
+	$(".list-group-item").first().addClass("active");
 $( ".list-group-item" ).click(function() {
   $(".list-group-item").removeClass( "list-group-item active" ).addClass( "list-group-item" );
   $( this ).addClass( "list-group-item active" );
+});
 });
--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,4 +1,5 @@
 class Mark < ActiveRecord::Base
+	validates :value, presence:true
   belongs_to :subject
   belongs_to :user
 end
